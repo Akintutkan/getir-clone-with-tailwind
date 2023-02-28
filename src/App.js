7a202d40808df7e7e5fc7c7c4d50be9e90587c44
Akin94 <akin.tutkan@gmail.com>
2023-02-28 14:17:47 +0300

    add responsive

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import Favorites from "./components/Favorites";
 import Footer from "./components/Footer";
 import MobileApp from "./components/MobileApp";
 import Categories from "./components/Categories";
+import { useWindowWidth } from '@react-hook/window-size'
 
 function App() {
+  const windowWidth = useWindowWidth()
+
   return (
     <>
     <Header/>
     <HeroSection />
       <Categories />
-      <Campaigns />
-      <div className="container mx-auto grid gap-y-6">
+      {windowWidth <= 768 && <Campaigns />}
+      <div className="container mx-auto grid gap-y-6 pt-8">
         <Favorites />
         <MobileApp />
         <Cards />
